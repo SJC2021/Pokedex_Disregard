fbb4f6073aa053f7db7cd0c9860c4382c3850a44
SJC2021 <82860116+SJC2021@users.noreply.github.com>
2021-05-28 10:02:21 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-
+let pokemonList = [{name: "Gengar", height: 1.5, types: ["fighting" ,"ghost"]},
+                   {name: "Hypno", height: 1.6, type: ["psychic"]},
+                   {name: "Breloom", height: 1.2, type: ["flying", "grass"}];
